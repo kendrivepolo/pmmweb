1163e1fcbc1bcf85d4bffa2934a1e37d8b73584b
Ken Hu <ken@mt.com.tw>
2015-12-31 18:35:20 +0800

    add testdata in MainCtrl.searchFilms() to test refresh page after searching films

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -43,9 +43,28 @@ angular.module('mymovies41FrontendApp')
 
 
    $scope.searchFilms = function () {
-       Searcher.searchFilms($scope.keyword).then(function (response) {
-           $scope.films = response.data;
-       });
+       var testdata = [
+           {"comment":"","description":"When her husband came from work today, Christine was there waiting for him with a little treat. She fed Logan grapes directly from her mouth, while she told him all the naughty things she wanted to do to him in bed. As Christine's hand traveled down Logan's flat stomach into his jeans, she discovered his member stiff and throbbing. His hardness turned her on so much, she wrapped her lips around the head, and pushed them to the base of the shaft. Once her snatch was so wet, Logan took her to bed and thrust himself deep into her from behind until a her sweet pussy clenched with orgasms.","id":4428,"owner":null,"performers":"Christine Paradise","rating":null,"streetDate":"12/27/2013","studio":{"filmCount":97,"id":306,"name":"Babes.com","url":"http://www.babes.com"},"title":"Take Me Down"},
+           {"comment":"","description":"Whenever Tyler Nixon comes home to find his girlfriend Tiffany Tyler wearing her high leather boots, he knows he's in for some really hot sex. This one night, Tiffany didn't waste any time sucking on Tyler's prick, licking at the crown like a lollipop, and sliding her wet lips up and down the shaft while playing with his balls. Then she climbed on top and straddled him like a cowgirl, bucking her hips as she rode him hard and fast. After Tiffany laid back to take his dick missionary-style, she tugged on his prick until he came all over her chest.","id":4429,"owner":null,"performers":"Tiffany Tyler","rating":null,"streetDate":"12/30/2013","studio":{"filmCount":97,"id":306,"name":"Babes.com","url":"http://www.babes.com"},"title":"In Her Mouth"},{"comment":"","description":"美女自摸，用根彎彎的棒子，自己捅自己。","id":4430,"owner":null,"performers":"Jasmin","rating":null,"streetDate":"2014.03.01","studio":{"filmCount":136,"id":302,"name":"WowGirls.com","url":"http://www.wowgirls.com"},"title":"Killing Time"},
+           {"comment":"","description":"","id":4507,"owner":null,"performers":"Emily Grey","rating":null,"streetDate":"2014-05-16","studio":{"filmCount":35,"id":117,"name":"18YearsOld.com","url":"http://18yearsold.com/h18/index.php?p=1"},"title":"Freaks in the Sheets"},
+           {"comment":"","description":"","id":4508,"owner":null,"performers":"Emily Grey","rating":null,"streetDate":"May 16, 2014","studio":{"filmCount":35,"id":117,"name":"18YearsOld.com","url":"http://18yearsold.com/h18/index.php?p=1"},"title":"Emily's Smooth Pussy"},
+           {"comment":"","description":"留下Scene 4：Denis Reed、Jenny Simons及Scene 5：Kattie Gold、Kristof Cale","id":4509,"owner":null,"performers":"","rating":null,"streetDate":"2014","studio":{"filmCount":33,"id":161,"name":"Daring","url":""},"title":"A Touch Of Nature"},
+           {"comment":"","description":"","id":4510,"owner":null,"performers":"Abigaile Johnson","rating":null,"streetDate":"","studio":{"filmCount":1,"id":354,"name":"First Anal Quest","url":"http://firstanalquest.com/"},"title":"College cutie does hardcore anal"}
+       ];
+
+       var i, l = testdata.length;
+       $scope.films = [];
+       $scope.filmChunks = [];
+
+       for ( i = 0; i < l; i += 3) {
+           console.log(i);
+           $scope.filmChunks.push( testdata.slice(i, i + 3) );
+       }
+
+       $scope.films = testdata;
+       //Searcher.searchFilms($scope.keyword).then(function (response) {
+       //    $scope.films = response.data;
+       //});
    };
 
     /**
